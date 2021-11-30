647694b612b6aaa8106c895795e840dbc4138dc1
tenznz <dhugkar@gmail.com>
2021-11-30 14:57:20 +0530

    Given message should return HAPPY

--- src/com/bridgelabz/mood_test_cases/MoodAnalyzerTest.java
+++ src/com/bridgelabz/mood_test_cases/MoodAnalyzerTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.mood_test_cases;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import com.bridgelabz.mood_analyzer.MoodAnalyzer;
@@ -10,16 +11,25 @@ public class MoodAnalyzerTest {
 
 	@BeforeClass
 	public static void init() {
-		System.out.println("Starting");
-
 		mood = MoodAnalyzer.getInstance();
 	}
 
 	@Test
 	public void given_message_should_return_SAD() {
 		String message = "I am in Sad Mood";
-
 		String actualMessage = mood.analyseMood(message);
 		assertEquals("SAD", actualMessage);
+	}
+
+	@Test
+	public void given_message_should_return_HAPPY() {
+		String message = "I am in Any Mood";
+		String actualMessage = mood.analyseMood(message);
+		assertEquals("HAPPY", actualMessage);
+	}
+
+	@AfterClass
+	public static void endTask() {
+
 	}
 }
